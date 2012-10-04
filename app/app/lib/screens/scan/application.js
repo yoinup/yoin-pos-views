bc37bf20648e0e7ef302b023ff578e52dc150d3e
edipotrebol <edipotrebol@gmail.com>
2012-10-04 12:50:34 +0200

    Adding has-error class to scan qr image + Update submodules

--- app/app/lib/screens/scan/application.js
+++ app/app/lib/screens/scan/application.js
@@ -4,7 +4,7 @@ App = Me.Application.create({
 	run: function() {
 
 		this._super();
-		Yvi.ScanScreenView.create().appendTo('#app');
+		Yvi.ScanScreenView.create( { hasError: false } ).appendTo('#app');
 
   }
 
