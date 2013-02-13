990dd0875dd6b3e72b9def4ebc899b6cb665ce58
Pepe Cano <ppcanodehuelva@gmail.com>
2013-02-13 19:53:45 +0100

    Precompile templates

---
+++ app/vendor/precompile/ember-handlebars-require.js
@@ -0,0 +1,18 @@
+
+
+(function(){ 
+  ENV = {
+    VIEW_PRESERVES_CONTEXT:true,
+    CP_DEFAULT_CACHEABLE:true
+  };
+})();
+ minispade.require('rsvp');
+ minispade.require('container');
+ minispade.require('ember-debug');
+ minispade.require('ember-metal');
+ minispade.require('ember-runtime');
+ minispade.require('ember-application');
+ minispade.require('ember-views');
+ minispade.require('ember-states');
+ minispade.require('metamorph');
+ minispade.require('ember-handlebars');
