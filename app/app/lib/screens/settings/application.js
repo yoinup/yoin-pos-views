f2ca51b41e995eb152e04332b2fa3f7bc63c29ad
Pepe Cano <ppcanodehuelva@gmail.com>
2013-05-24 18:45:08 +0200

    Change parmas

--- app/app/lib/screens/settings/application.js
+++ app/app/lib/screens/settings/application.js
@@ -4,12 +4,25 @@ App = Me.Application.create({
 	run: function() {
 
 		this.loadData();
-		var self = this;
 
-		self.view = Yvi.SettingsScreenView.create({
+		var self = this,
+				brand = this.store.find(Yn.Brand, 21),
+				cities = this.store.findMany(Yn.City, [9,10,11]);
+				venue = this.store.find(Yn.Venue, 236);
+
+		venue.addObserver('isLoaded', function() {
+
+			self.view = Yvi.SettingsScreenView.create({
+				venue: venue,
+				brand: brand,
+				cities: cities,
+				showCameraDeviceButton: true,
+				isDeviceFront: true
+			});
+
+			self.view.appendTo('#app');
 		});
 
-		self.view.appendTo('#app');
 
 	}
 
